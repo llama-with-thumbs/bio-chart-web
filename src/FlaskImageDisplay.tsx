929ad33864a2eccc83b9d16a524b8bbdb213920f
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-11-17 12:31:49 -0500

    fix: display minutes, hours, and days

--- src/FlaskImageDisplay.tsx
+++ src/FlaskImageDisplay.tsx
@@ -15,53 +15,61 @@ interface FlaskImageDisplayProps {
   flask: Flask;
 }
 
+const calculateTimeAgo = (differenceInMinutes: number): [string, number] => {
+  if (differenceInMinutes < 60) {
+    return ['minute', differenceInMinutes];
+  } else if (differenceInMinutes < 1440) {
+    return ['hour', Math.floor(differenceInMinutes / 60)];
+  } else {
+    return ['day', Math.floor(differenceInMinutes / 1440)];
+  }
+};
+
+const renderTimeAgo = (unit: string, value: number) => (
+  <span>
+    <span style={{ fontWeight: 'bold', fontSize: '16px' }}>
+      {value}</span> {unit}{value !== 1 ? 's' : ''} ago
+  </span>
+);
+
 const FlaskImageDisplay: React.FC<FlaskImageDisplayProps> = ({ flask }) => {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
   const [minutesAgo, setMinutesAgo] = useState<number | null>(null);
 
   useEffect(() => {
-    // Initialize Firebase Storage
     const storage = getStorage();
-
-    // Create a reference to the most recent image
     const imageRef = ref(storage, flask.most_recent_snippet_path);
 
-    // Get the download URL for the image
-    getDownloadURL(imageRef)
-      .then((url) => setImageUrl(url))
-      .catch((error) => console.error('Error getting download URL:', error));
+    const updateMinutesAgo = () => {
+      const lastUpdateDate = new Date(flask.last_update);
+      const currentDate = new Date();
+      const differenceInMinutes = Math.floor((currentDate.getTime() - lastUpdateDate.getTime()) / (1000 * 60));
+      setMinutesAgo(differenceInMinutes);
+    };
 
-    // Calculate minutes ago initially
+    // Initial update
     updateMinutesAgo();
 
     // Set up interval to update minutes every minute
     const intervalId = setInterval(updateMinutesAgo, 60000);
 
-    // Clear interval on component unmount
+    // Cleanup interval on component unmount
     return () => clearInterval(intervalId);
   }, [flask.most_recent_snippet_path, flask.last_update]);
 
-  const updateMinutesAgo = () => {
-    const lastUpdateDate = new Date(flask.last_update);
-    const currentDate = new Date();
-    const differenceInMinutes = Math.floor((currentDate.getTime() - lastUpdateDate.getTime()) / (1000 * 60));
-  
-    if (differenceInMinutes < 60) {
-      setMinutesAgo(differenceInMinutes);
-    } else if (differenceInMinutes < 1440) {
-      const differenceInHours = Math.floor(differenceInMinutes / 60);
-      setMinutesAgo(differenceInHours);
-    } else {
-      const differenceInDays = Math.floor(differenceInMinutes / 1440);
-      setMinutesAgo(differenceInDays);
-    }
-  };
-  
+  useEffect(() => {
+    const storage = getStorage();
+    const imageRef = ref(storage, flask.most_recent_snippet_path);
+
+    getDownloadURL(imageRef)
+      .then((url) => setImageUrl(url))
+      .catch((error) => console.error('Error getting download URL:', error));
+  }, [flask.most_recent_snippet_path]);
 
   return (
-    <div style={{ position: 'relative', padding: '0px', margin: '0px', display: 'flex' }}>
+    <div style={{ position: 'relative', padding: '0', margin: '0', display: 'flex' }}>
       {imageUrl && (
-        <div style={{ position: 'relative' }}>
+        <div style={{ position: 'relative', display: 'flex' }}>
           <img
             src={imageUrl}
             alt={`Image for ${flask.flask}`}
@@ -80,14 +88,12 @@ const FlaskImageDisplay: React.FC<FlaskImageDisplayProps> = ({ flask }) => {
               left: '5px',
               backgroundColor: 'rgba(255, 0, 0, 0)',
               color: '#00ff00',
-              margin: 0,
+              margin: '0',
               padding: '5px',
-              fontSize: '14px',
-              // fontWeight: 'bold',
-              letterSpacing: '-.5px'
+              fontSize: '12px',
             }}
           >
-            Last Update: {minutesAgo !== null ? <span><strong>{minutesAgo} </strong> min ago</span>: 'N/A'}
+            Last Update: {minutesAgo !== null ? renderTimeAgo(...calculateTimeAgo(minutesAgo)) : 'N/A'}
           </p>
         </div>
       )}
